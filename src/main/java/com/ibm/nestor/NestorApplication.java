9aeb6893dc302a15d81484e8359c933a0a6d22c3
rohitkshetty <rohit.k.shetty@gmail.com>
2020-08-19 20:38:24 -0400

    Application

---
+++ src/main/java/com/ibm/nestor/NestorApplication.java
@@ -0,0 +1,13 @@
+package com.ibm.nestor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NestorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NestorApplication.class, args);
+	}
+
+}
