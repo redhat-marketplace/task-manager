2001d742f02ffd54199484b14c63cbb920eefcc1
rohitkshetty <rohit.k.shetty@gmail.com>
2020-08-21 16:24:47 -0400

    test

---
+++ src/test/java/com/ibm/nestor/NestorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ibm.nestor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NestorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
